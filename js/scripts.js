edd05541cb215e0b68da7b900a24d0ee8eb94ac4
Kyle Tenzeldam and Ross Fletcher <ross@>
2017-12-04 08:14:19 -0800

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+var name = $(#name).val();
+var car = $(#month).val();
+var street = $(#street).val();
+var color = $(#color).val();
+
+
+
+
+  $("form#questions").submit(function() {
+
+  });
+});
