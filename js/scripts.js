06f719f8d78e85ab34d3392eb92105b456f902d6
Kyle Tenzeldam and Ross Fletcher <ross@>
2017-12-04 09:03:13 -0800

    add js to push ul arrays to list

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,22 @@
 $(document).ready(function(){
-
-var name = $(#name).val();
-var car = $(#month).val();
-var street = $(#street).val();
-var color = $(#color).val();
-
-
-
-
   $("form#questions").submit(function() {
+    var name = $("input#name").val();
+    var car = $("input#car").val();
+    var street = $("input#street").val();
+    var color = $("input#color").val();
+    var month = $("input#month").val();
 
+    var answerArray = [name, car, month, street, color];
+    // alert(answerArray);
+    var secondArray = answerArray.slice();
+    var ulone = secondArray[1];
+    var ultwo = secondArray[0];
+    var ulthree = secondArray[2];
+
+    $("#answer1").text(ulone)
+    $("#answer2").text(ultwo)
+    $("#answer3").text(ulthree)
+
+    event.preventDefault();
   });
 });
